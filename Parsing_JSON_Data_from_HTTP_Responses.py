35eb7cb7eff3f48a077ecdf74212c65ae231c900
sakthi vignesh <sakthi007vicky007@gmail.com>
2024-10-03 17:52:51 +0530

    fourth program

---
+++ Parsing_JSON_Data_from_HTTP_Responses.py
@@ -0,0 +1,17 @@
+import requests
+
+url="https://jsonplaceholder.typicode.com/posts"
+
+response=requests.get(url)
+
+if response.status_code==200:
+    data=response.json()
+    first=data[0]
+    id=first.get('id')
+    print(id)
+
+    for i in data:
+        print(f"Post no :{i.get('id')}")
+else:
+    print("error")
+
